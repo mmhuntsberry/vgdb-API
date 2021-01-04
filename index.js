74f9a15066bacc02fc2f17a977a427df95f8385f
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2021-01-04 14:49:49 -0700

    Chore: Create mySQL connection.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const pool = require('./sql/connection');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  // res.send('Hello World!')
+  pool.query('SELECT * FROM games', (err, rows) => {
+    if(err) {
+      console.log({err});
+      return res.status(500).send('An unexpected error occurred.')
+    }
+    res.json(rows)
+  })
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
---
+++ sql/connection.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+require('dotenv').config()
+
+class Connection {
+  constructor() {
+    if(!this.pool) {
+      console.log('Creating connection pool');
+      this.pool = mysql.createPool({
+        connectionLimit: 100,
+        host: process.env.DB_HOSTNAME,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_SCHEMA
+      })
+
+      return this.pool
+    }
+
+    return this.pool
+  }
+}
+
+const instance = new Connection();
+
+module.exports = instance;
