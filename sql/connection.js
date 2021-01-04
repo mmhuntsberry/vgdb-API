1e4ba8c760073f3766583a8b2522611d4559c6ed
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2021-01-04 15:01:24 -0700

    Chore: Setup prettierrc file.

--- sql/connection.js
+++ sql/connection.js
@@ -1,22 +1,22 @@
-const mysql = require('mysql');
-require('dotenv').config()
+const mysql = require("mysql");
+require("dotenv").config();
 
 class Connection {
   constructor() {
-    if(!this.pool) {
-      console.log('Creating connection pool');
+    if (!this.pool) {
+      console.log("Creating connection pool");
       this.pool = mysql.createPool({
         connectionLimit: 100,
         host: process.env.DB_HOSTNAME,
         user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_SCHEMA
-      })
+        database: process.env.DB_SCHEMA,
+      });
 
-      return this.pool
+      return this.pool;
     }
 
-    return this.pool
+    return this.pool;
   }
 }
 
