daf280f1518c4fa88e76e5ba143023ae729a9dee
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2021-01-04 15:00:24 -0700

    Chore: Setup initialize and insert sql files.

---
+++ sql/initialize.sql
@@ -0,0 +1,7 @@
+CREATE TABLE games (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  release_year INT NOT NULL,
+  box_art VARCHAR(255) NOT NULL,
+  synopsis TEXT NOT NULL,
+);
---
+++ sql/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO games (title, release_year, box_art, synopsis)
+VALUES (
+    "Super Mario Bros.",
+    1985,
+    "https://images.igdb.com/igdb/image/upload/t_cover_big/co2362.jpg",
+    "A side scrolling 2D platformer and first entry in the Super Mario franchise, Super Mario Bros. follows Italian plumber Mario as he treks across many levels of platforming challenges featuring hostile enemies to rescue Princess Peach from the evil king Bowser."
+  )
