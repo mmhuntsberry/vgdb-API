88247d1507b4912a50643a9d7e5b3b703a641fba
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2021-01-07 11:36:25 -0700

    Update: insert script

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,4 +1,12 @@
-INSERT INTO games (title, release_year, box_art, synopsis)
+INSERT INTO games (
+    title,
+    release_year,
+    box_art,
+    synopsis,
+    trailer,
+    hero,
+    developer
+  )
 VALUES (
     "Super Mario Bros.",
     1985,
@@ -13,8 +21,8 @@ VALUES (
     1983,
     "https://images.igdb.com/igdb/image/upload/t_cover_big/z6xrk4qkqerfdrvsgkin.jpg",
     "The game is viewed in a top-down perspective and is vertical scrolling. The player does not select plays for either offense or defense. On offense, the player simply receives the ball upon the snap and either attempt to run with the quarterback, toss the ball to a running back, or throw the ball to the one long distance receiver - basically the option offense. On defense, the player chooses one of two players to control, and the computer manipulates the others. The ball can also be punted or a field goal can be attempted.
-
-10-Yard Fight has five levels of difficulty; from easiest to most difficult: high school, college, professional, playoff, and Super Bowl. If the player wins both halves of an 'accelerated real time' 30-minute half at an easier level, the player advanced to the next level of difficulty, like a career mode." "https://youtu.be/5LUyTv9j1TM",
+10-Yard Fight has five levels of difficulty; from easiest to most difficult: high school, college, professional, playoff, and Super Bowl. If the player wins both halves of an 'accelerated real time' 30-minute half at an easier level, the player advanced to the next level of difficulty, like a career mode.",
+    "https://youtu.be/5LUyTv9j1TM",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmdvA0pCJVM6q_Vqig80YdIXZuqIDTWjQouA&usqp=CAU",
     "Irem"
   ),
@@ -24,12 +32,17 @@ VALUES (
     "https://images.igdb.com/igdb/image/upload/t_cover_big/pbxb9zyymc21ijapkucu.jpg",
     NULL,
     "https://youtu.be/jTbH25YNxE4",
+    "https://i0.wp.com/thewellredmage.com/wp-content/uploads/2016/11/end.png?resize=499%2C421&ssl=1",
+    "Square"
   ),
   (
     "1942",
     1984,
     "https://images.igdb.com/igdb/image/upload/t_cover_big/co1w4l.jpg",
-    "1942 is a vertically scrolling shoot 'em up made by Capcom that was released for the arcade in 1984. The goal is to reach Tokyo and destroy the entire Japanese air fleet. The player pilots a plane dubbed the 'Super Ace' . The player has to shoot down enemy planes; to avoid enemy fire, the player can perform a roll or 'loop-the-loop'. During the game the player may collect a series of power-ups, one of them allowing the plane to be escorted by two other smaller fighters in a Tip Tow formation."
+    "1942 is a vertically scrolling shoot 'em up made by Capcom that was released for the arcade in 1984. The goal is to reach Tokyo and destroy the entire Japanese air fleet. The player pilots a plane dubbed the 'Super Ace' . The player has to shoot down enemy planes; to avoid enemy fire, the player can perform a roll or 'loop-the-loop'. During the game the player may collect a series of power-ups, one of them allowing the plane to be escorted by two other smaller fighters in a Tip Tow formation.",
+    "https://youtu.be/zKAe-Y579g0",
+    "https://i.ytimg.com/vi/iXfuUAFjVE0/maxresdefault.jpg",
+    "Capcom"
   ),
   (
     "1943: The Battle of Midway",
@@ -37,7 +50,10 @@ VALUES (
     "https://images.igdb.com/igdb/image/upload/t_cover_big/co1qm0.jpg",
     "The game is set in the Pacific theater of World War II, off the coast of the Midway Atoll. The goal is to attack the Japanese Air Fleet that bombed the players' American Aircraft Carrier, pursue all Japanese Air and Sea forces, fly through the 16 levels of play, make their way to the Japanese battleship Yamato and destroy her. 11 Levels consist of an Air-to-Sea battle (with a huge battleship or an aircraft carrier as an End-Level Boss), while 5 levels consist of an all-aerial battle against a squadron of Japanese Bombers and a Mother Bomber that needs to be destroyed.
 
-As in 1942, players pilot a P-38. Two buttons are used: one for regular attacks (with several weapons) and one for special actions that executes either a loop like in 1942 or one of three special attacks that damage the plane. Unlike 1942, the player only has one life, with one refillable energy meter. Destroying a complete formation of red enemy planes will result in a power-up, such as a health boost or a temporary special weapons which replaces the default twin gun."
+As in 1942, players pilot a P-38. Two buttons are used: one for regular attacks (with several weapons) and one for special actions that executes either a loop like in 1942 or one of three special attacks that damage the plane. Unlike 1942, the player only has one life, with one refillable energy meter. Destroying a complete formation of red enemy planes will result in a power-up, such as a health boost or a temporary special weapons which replaces the default twin gun.",
+    "https://youtu.be/FSuv5BAkk4s",
+    "https://i2.wp.com/nintendotimes.com/wp-content/uploads/2018/09/1943-Title.jpg?resize=800%2C445&ssl=1",
+    "Capcom"
   ),
   (
     "8 Eyes",
@@ -49,29 +65,17 @@ These strange jewels of power were formed at the eyes, or centers, of the eight 
 
 The task of retrieving the 8 Eyes falls to you, Orin the Falconer, the bravest and mightiest of the King's Guardsmen. With your fighting falcon, Cutrus, you must penetrate each of the eight Dukes' castles. There you will face the Dukes' soldiers, and battle strange nuclear mutants such as living skeletons, giant wasps, and mud men. You must defeat the monstrous Boss of each castle to retrieve the Jewel of Power he guards.
 
-Then, to complete your quest, return the 8 Eyes to the Altar of Peace to await the return on the Great King, so that he may finish the rebuilding of Earth. Your reward will be the the eternal gratitude of all mankind!"
+Then, to complete your quest, return the 8 Eyes to the Altar of Peace to await the return on the Great King, so that he may finish the rebuilding of Earth. Your reward will be the the eternal gratitude of all mankind!",
+    "https://youtu.be/8TVrMSuQrFQ",
+    "https://i.ytimg.com/vi/Lmy_zkykBtI/maxresdefault.jpg",
+    "Thinking Rabbit"
   ),
   (
     "A Boy and His Blob",
     1989,
     "https://images.igdb.com/igdb/image/upload/t_cover_big/rgbnby0hsennxbflob6j.jpg",
-    "A Boy and His Blob: Trouble on Blobolonia is a 1989 video game developed by Imagineering for the Nintendo Entertainment System (NES). The game was published by Absolute Entertainment in North America and Europe and by Jaleco in Japan. A Boy and His Blob follows an unnamed, male protagonist and his shapeshifting blob friend on their adventure to save the planet of Blobolonia from the clutches of an evil emperor."
-  ),
-  (
-    "Acclaim's Star Voyager",
-    1986,
-    "https://images.igdb.com/igdb/image/upload/t_cover_big/cixn5seyzqvvx61ycxga.jpg",
-    NULL
-  ),
-  (
-    "Abadox: The Deadly Inner War",
-    1988,
-    "https://images.igdb.com/igdb/image/upload/t_cover_big/co2cia.jpg",
-    "Abadox is a video game for the NES, subtitled The Deadly Inner War. It is a side-scrolling shoot 'em up in the vein of Gradius and R-Type. The game is notable for its unique visual design, as the game takes place inside the intestinal tract of a giant alien organism. Abadox is also known to be difficult, since it takes one hit from an enemy projectile to be killed. In Abadox, if a player is killed, one must restart from a checkpoint passed before death."
-  ),
-  (
-    "720 degrees",
-    1986,
-    "https://images.igdb.com/igdb/image/upload/t_cover_big/co1qly.jpg",
-    "720 Degrees is a 1986 arcade game by Atari Games. 720°, a skateboarding game, is notable in that along with BMX Simulator, it is one of the first extreme sports video games, and has a unique timed structure that requires the player score points in order to keep the game going."
+    "A Boy and His Blob: Trouble on Blobolonia is a 1989 video game developed by Imagineering for the Nintendo Entertainment System (NES). The game was published by Absolute Entertainment in North America and Europe and by Jaleco in Japan. A Boy and His Blob follows an unnamed, male protagonist and his shapeshifting blob friend on their adventure to save the planet of Blobolonia from the clutches of an evil emperor.",
+    "https://youtu.be/QcWajywEfE0",
+    "https://i.pinimg.com/originals/93/ed/e9/93ede936a886324dcb64d8f8f2a99f91.jpg",
+    "Imagineer"
   )
